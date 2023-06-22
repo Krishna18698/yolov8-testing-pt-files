016e27687795886c71e77d54269651c21fbbd508
Krishna18698 <73110324+Krishna18698@users.noreply.github.com>
2023-06-23 03:27:24 +0530

    Update test4.py

--- New folder/test4.py
+++ New folder/test4.py
@@ -9,9 +9,27 @@ from pathlib import Path
 import sqlite3 as sl
 import time
 import argparse
+
+video_source = ""
+video = False
+model = "nanoyolov8.blob"
+customModel = True
+sizeX = 640
+sizeY = 640
 sizeX=640
 sizeY=640
 
+parentDir = Path(__file__).parent
+
+outputFrame = None
+lock = threading.Lock()
+
+labelMap = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+            "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair dryer", "toothbrush"]
+
+with open('../../data/yolov8n.json') as f:
+    config = json.load(f)
+    
 def init_pipeline():
     pipeline = depthai.Pipeline()
     cam_rgb = pipeline.createColorCamera()
